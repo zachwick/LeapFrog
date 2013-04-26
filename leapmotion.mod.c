7f0f99cabf088195438d48b4d5a4634fb3a1224d
Zach Wick <zach@zachwick.com>
2013-04-26 09:29:11 -0400

    Erroneously added licensing information to this file.

--- leapmotion.mod.c
+++ leapmotion.mod.c
@@ -1,26 +1,3 @@
-/********************************************
- * LeapFrog
- * ========
- * An open-source Linux character driver for
- * the Leap Motion sensor.
- * --------------------------------
- * Copyright 2013 Princeton Ferro
- *
- * This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
-*********************************************/
-
 #include <linux/module.h>
 #include <linux/vermagic.h>
 #include <linux/compiler.h>
@@ -29,17 +6,28 @@ MODULE_INFO(vermagic, VERMAGIC_STRING);
 
 struct module __this_module
 __attribute__((section(".gnu.linkonce.this_module"))) = {
-	.name = KBUILD_MODNAME,
-	.init = init_module,
+ .name = KBUILD_MODNAME,
+ .init = init_module,
 #ifdef CONFIG_MODULE_UNLOAD
-	.exit = cleanup_module,
+ .exit = cleanup_module,
 #endif
-	.arch = MODULE_ARCH_INIT,
+ .arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0x568fba06, "module_layout" },
+	{ 0x335879f5, "usb_deregister" },
+	{ 0x9d13d499, "usb_register_driver" },
+	{ 0x7c3c2550, "usb_register_dev" },
+	{ 0x6f8423e7, "usb_deregister_dev" },
+	{ 0x27e1a049, "printk" },
 };
 
 static const char __module_depends[]
 __used
 __attribute__((section(".modinfo"))) =
-"depends=";
+"depends=usbcore";
 
-MODULE_ALIAS("usb:vF182p0003d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:vF182p0003d*dc*dsc*dp*ic*isc*ip*");
